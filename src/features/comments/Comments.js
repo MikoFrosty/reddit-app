a8871c88175f02289a51e55c1fd5825c885872f9
brandon <srlguitarist@gmail.com>
2022-10-29 19:39:50 -0400

    Add skeletons for when loading

--- src/features/comments/Comments.js
+++ src/features/comments/Comments.js
@@ -3,45 +3,62 @@ import { useSelector } from "react-redux";
 import { selectCommentsStatus } from "../../app/redditSlice";
 
 export default function Comments({ comments = [] }) {
-    const commentsStatus = useSelector(selectCommentsStatus) || "idle";
-    
-    const commentsContainerStyle = {
-        position: 'relative',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        maxWidth: 700,
-        width: '100%',
-        zIndex: '100',
-    };
+  const commentsStatus = useSelector(selectCommentsStatus) || "idle";
 
-    const commentStyle = {
-        backgroundColor: '#f5f5f5',
-        padding: 10,
-        margin: 5,
-        width: '100%',
-        height: 'auto',
-        textAlign: 'left',
-        border: '1px solid #ccc',
-        borderRadius: 5,
-        boxShadow: '0 0 5px #ccc',
-        overflowWrap: 'break-word',
-    };
+  const commentsContainerStyle = {
+    position: "relative",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    maxWidth: 700,
+    width: "100%",
+    zIndex: "100",
+  };
 
-    if (commentsStatus === "loading") {
-        return (
-            <div style={commentsContainerStyle}>
-                <Skeleton count={5} containerClassName="skeleton-container" />
-            </div>
-        );
-    };
+  const commentStyle = {
+    backgroundColor: "#202020",
+    padding: 10,
+    margin: 5,
+    width: "100%",
+    height: "auto",
+    textAlign: "left",
+    border: "1px solid #505050",
+    borderRadius: 5,
+    boxShadow: "0 0 5px #000",
+    overflowWrap: "break-word",
+  };
 
+  if (commentsStatus === "loading") {
     return (
-        <div style={commentsContainerStyle}>
-            {commentsStatus === "idle" && comments.map((comment) => (
-                <p style={commentStyle} key={comment.id}>{comment.body}</p>
-            ))}
-        </div>
-    )
+      <div style={commentsContainerStyle}>
+        <Skeleton
+          count={5}
+          containerClassName="skeleton-container"
+          height="40px"
+          baseColor="#252525"
+          highlightColor="#505050"
+          style={{ border: "1px solid #505050", marginBottom: 5 }}
+        />
+      </div>
+    );
+  }
+
+  if (commentsStatus === "rejected") {
+    return (
+      <div style={commentsContainerStyle}>
+        <p>Failed to load comments | Please try again later</p>
+      </div>
+    );
+  }
+
+  return (
+    <div style={commentsContainerStyle}>
+      {comments.map((comment) => (
+        <p style={commentStyle} key={comment.id}>
+          {comment.body}
+        </p>
+      ))}
+    </div>
+  );
 }
