4d0618e9245c8b7cb3e9be9453583bd0e804a438
brandon <srlguitarist@gmail.com>
2022-10-29 12:54:11 -0400

    edit (file due to be deleted)

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -14,13 +14,14 @@ export function Counter() {
   const count = useSelector(selectCount);
   const dispatch = useDispatch();
   const [incrementAmount, setIncrementAmount] = useState('2');
-
+  
   const incrementValue = Number(incrementAmount) || 0;
 
   return (
-    <div>
+    <div data-testid="counter">
       <div className={styles.row}>
         <button
+          data-testid="decrement"
           className={styles.button}
           aria-label="Decrement value"
           onClick={() => dispatch(decrement())}
@@ -29,6 +30,7 @@ export function Counter() {
         </button>
         <span className={styles.value}>{count}</span>
         <button
+          data-testid="increment"
           className={styles.button}
           aria-label="Increment value"
           onClick={() => dispatch(increment())}
@@ -38,24 +40,28 @@ export function Counter() {
       </div>
       <div className={styles.row}>
         <input
+          data-testid="incrementAmount"
           className={styles.textbox}
           aria-label="Set increment amount"
           value={incrementAmount}
           onChange={(e) => setIncrementAmount(e.target.value)}
         />
         <button
+          data-testid="incrementByAmount"
           className={styles.button}
           onClick={() => dispatch(incrementByAmount(incrementValue))}
         >
           Add Amount
         </button>
         <button
+          data-testid="incrementAsync"
           className={styles.asyncButton}
           onClick={() => dispatch(incrementAsync(incrementValue))}
         >
           Add Async
         </button>
         <button
+          data-testid="incrementIfOdd"
           className={styles.button}
           onClick={() => dispatch(incrementIfOdd(incrementValue))}
         >
