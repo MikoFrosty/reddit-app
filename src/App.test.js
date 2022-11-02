9c914ddad6f0151bcafb3511f396d5e6300d6fad
brandon <srlguitarist@gmail.com>
2022-11-01 20:27:17 -0400

    Add basic app testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./App";
 
-it('renders without crashing', () => {
-  render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-}); 
+describe("App", () => {
+  it("renders without crashing", () => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+  });
+  it("renders the header", () => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+    expect(screen.getByText("Reddit For You")).toBeInTheDocument();
+  });
+});
+
