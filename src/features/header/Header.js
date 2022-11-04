0571f417431ace685a92ef028470abacbf780d99
brandon <srlguitarist@gmail.com>
2022-11-03 23:46:31 -0400

    Move styles from inline to module file. Edit logoclick function to also scroll user to top of page

--- src/features/header/Header.js
+++ src/features/header/Header.js
@@ -1,60 +1,18 @@
 import RedditCategorySelector from "./RedditCategorySelector";
+import headerStyles from "./Header.module.css";
 
 export default function Header() {
   function handleLogoClick() {
     window.location.reload();
+    window.scrollTo(0, 0);
   }
 
-  const headerOuterContainerStyle = {
-    position: "fixed",
-    top: 0,
-    left: 0,
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    width: "100%",
-    height: 60,
-    zIndex: "1000",
-    backgroundColor: "#202020",
-    boxShadow: "0 0 5px #000",
-    borderBottom: "1px solid #505050",
-  };
-
-  const headerInnerContainerStyle = {
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-around",
-    width: "100%",
-    maxWidth: 800,
-    height: "100%",
-    color: "#fff",
-  };
-
-  const h1Style = {
-    fontSize: "min(1.4rem, 4.3vw)",
-    fontWeight: "bold",
-    minWidth: "min(155px, 1vw)",
-  };
-
-  const logoWrapperStyle = {
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    cursor: "pointer",
-  };
-
   return (
-    <div style={headerOuterContainerStyle}>
-      <div style={headerInnerContainerStyle}>
-        <div style={logoWrapperStyle} onClick={handleLogoClick}>
-          <img
-            src="./logo500.png"
-            alt="reddit logo"
-            style={{ width: 40, height: 40, margin: 3 }}
-          />
-          <h1 style={h1Style}>Reddit For You</h1>
+    <div id={headerStyles["header-outer-container"]}>
+      <div id={headerStyles["header-inner-container"]}>
+        <div id={headerStyles["logo-wrapper"]} onClick={handleLogoClick}>
+          <img src="./logo500.png" alt="reddit logo" />
+          <h1>Reddit For You</h1>
         </div>
         <RedditCategorySelector />
       </div>
