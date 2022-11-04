191c8155f0bf694018761e873d89781c370d62ad
brandon <srlguitarist@gmail.com>
2022-11-03 23:46:44 -0400

    Move inline styles to module file

--- src/features/footer/Footer.js
+++ src/features/footer/Footer.js
@@ -1,20 +1,9 @@
+import footerStyles from "./Footer.module.css";
+
 export default function Footer() {
-
-    const footerContainerStyle = {
-        position: 'relative',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: '100%',
-        height: 70,
-        zIndex: '100',
-        color: '#fff',
-    };
-
-    return (
-        <div style={footerContainerStyle}>
-            <p>Created by Brandon Mikowski</p>
-        </div>
-    )
+  return (
+    <div id={footerStyles["footer-container"]}>
+      <p>Created by Brandon Mikowski</p>
+    </div>
+  );
 }
