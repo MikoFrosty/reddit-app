055be0cf56c3464e1c088706cf88d66fd7f72bb2
brandon <srlguitarist@gmail.com>
2022-11-04 23:14:39 -0400

    edit style import name

--- src/features/comments/Comments.js
+++ src/features/comments/Comments.js
@@ -1,14 +1,14 @@
 import Skeleton from "react-loading-skeleton";
 import { useSelector } from "react-redux";
 import { selectCommentsStatus } from "../../app/redditSlice";
-import commentsStyle from "./Comments.module.css";
+import commentsStyles from "./Comments.module.css";
 
 export default function Comments({ comments = [] }) {
   const commentsStatus = useSelector(selectCommentsStatus) || "idle";
 
   if (commentsStatus === "loading") {
     return (
-      <div id={commentsStyle["comments-container"]}>
+      <div id={commentsStyles["comments-container"]}>
         <Skeleton
           count={5}
           containerClassName="skeleton-container"
@@ -23,16 +23,16 @@ export default function Comments({ comments = [] }) {
 
   if (commentsStatus === "rejected") {
     return (
-      <div id={commentsStyle["comments-container"]}>
+      <div id={commentsStyles["comments-container"]}>
         <p>Failed to load comments | Please try again later</p>
       </div>
     );
   }
 
   return (
-    <div id={commentsStyle["comments-container"]}>
+    <div id={commentsStyles["comments-container"]}>
       {comments.map((comment) => (
-        <p className={commentsStyle.comment} key={comment.id}>
+        <p className={commentsStyles.comment} key={comment.id}>
           {comment.body}
         </p>
       ))}
